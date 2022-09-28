87d5ca6a0ff67220634b5288d87f28adb809e42d
melegithubyit <111082914+melegithubyit@users.noreply.github.com>
2022-09-28 08:13:16 -0700

    Add files via upload

--- Fibonacci Number.py
+++ Fibonacci Number.py
@@ -2,7 +2,4 @@ class Solution(object):
     def fib(self, n):
         if n == 0 or n==1:
             return n
-        return fib(self,n - 1) + fib(self,n - 2)
-
-trial=Solution()
-print(trial.fib(3))
+        return self.fib(n - 1) + self.fib(n - 2)
