a908845b7c339abb6655819d00bb6b2f061acf88
Melakeselam Yitbarek <111082914+melegithubyit@users.noreply.github.com>
2023-06-03 20:40:31 +0300

    Add files via upload

---
+++ current_smaller.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def smallerNumbersThanCurrent(self, nums):
+        test = []
+        for i in range(len(nums)):
+            counter = 0
+            for j in range(len(nums)):
+                if nums[i] > nums[j]:
+                    counter += 1
+            test.append(counter)
+
+        return test
