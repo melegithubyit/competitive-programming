cae3b8efa51ec2136655baa4d0d41645cc5737db
Melakeselam Yitbarek <111082914+melegithubyit@users.noreply.github.com>
2023-06-30 22:34:51 +0300

    Add files via upload

---
+++ sort_colors.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sortColors(nums) -> None:
+        red, white, blue = 0, 0, len(nums) - 1
+
+        while white <= blue:
+            if nums[white] == 0:
+                nums[red], nums[white] = nums[white], nums[red]
+                red += 1
+                white += 1
+            elif nums[white] == 1:
+                white += 1
+            else:
+                nums[white], nums[blue] = nums[blue], nums[white]
+                blue -= 1
