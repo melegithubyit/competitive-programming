6f31842ea208a755141b7f0284dea5181e5c755e
Melakeselam Yitbarek <111082914+melegithubyit@users.noreply.github.com>
2023-07-11 15:23:05 +0300

    Add files via upload

---
+++ merge_two_sortedLiost.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(list1, list2):
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        dummy = ListNode(0)
+        current = dummy
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+        if list1:
+            current.next = list1
+        if list2:
+            current.next = list2
+        return dummy.next
