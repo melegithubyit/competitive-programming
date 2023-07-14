42ec0c1cf0aec2eb5daff74c04300af4a1d11714
Melakeselam Yitbarek <111082914+melegithubyit@users.noreply.github.com>
2023-07-14 14:29:34 +0300

    Add files via upload

---
+++ remove_duplicated_from_sorted_list.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(head):
+        if not head:
+            return None
+        current = head
+        while current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+        return head
