9f397be53f1184632a37a0d803ba85894c2e0d71
Melakeselam Yitbarek <111082914+melegithubyit@users.noreply.github.com>
2023-07-21 15:51:14 +0300

    Add files via upload

--- sortList.py
+++ sortList.py
@@ -1,5 +1,10 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
-    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def sortList(self, head):
         start = head
 
         nums = []
