f436a38b8ca139577a5a46ddf18d8dc178f42175
Melakeselam Yitbarek <111082914+melegithubyit@users.noreply.github.com>
2023-08-29 22:30:55 +0300

    Add files via upload

---
+++ get_equal_substring_within_budget.py
@@ -0,0 +1,17 @@
+class Solution:
+    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+        n = len(s)
+        left = 0
+        max_length = 0
+        current_cost = 0
+
+        for right in range(n):
+            current_cost += abs(ord(s[right]) - ord(t[right]))
+
+            while current_cost > maxCost:
+                current_cost -= abs(ord(s[left]) - ord(t[left]))
+                left += 1
+
+            max_length = max(max_length, right - left + 1)
+
+        return max_length
