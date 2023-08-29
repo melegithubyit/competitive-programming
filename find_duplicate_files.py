ff0d1ceea79aa838eca6cc1dde660ce40cb5aa2c
Melakeselam Yitbarek <111082914+melegithubyit@users.noreply.github.com>
2023-08-29 22:13:32 +0300

    Add files via upload

---
+++ find_duplicate_files.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findDuplicate(self, paths):
+        file_dict = {}
+        for path in paths:
+            directory, *files = path.split()
+            for file in files:
+                file_name, content = file.split('(')
+                file_dict.setdefault(content, []).append(
+                    directory + '/' + file_name)
+        return [file_paths for file_paths in file_dict.values() if len(file_paths) > 1]
