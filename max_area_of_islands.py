71c62beb2c0cace777881d59482af700e576c724
Melakeselam Yitbarek <111082914+melegithubyit@users.noreply.github.com>
2023-09-22 18:13:46 +0300

    Add files via upload

---
+++ max_area_of_islands.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+
+        def dfs(i, j):
+            if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] == 0:
+                return 0
+
+            grid[i][j] = 0
+            return 1 + dfs(i+1, j) + dfs(i-1, j) + dfs(i, j+1) + dfs(i, j-1)
+
+        areas = []
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    areas.append(dfs(i, j))
+
+        return max(areas) if areas else 0
