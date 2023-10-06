1435c186c0bcbae2f9c6e049a905902cc47da4c1
Melakeselam Yitbarek <111082914+melegithubyit@users.noreply.github.com>
2023-10-06 18:17:16 +0300

    Add files via upload

---
+++ max_coin_you_can_get.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+class Solution:
+    def maxCoins(self, piles):
+        piles.sort()
+        piles = deque(piles)
+        total = 0
+        while piles:
+            piles.pop()
+            total += piles.pop()
+            piles.popleft()
+        return total
