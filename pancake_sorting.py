8a81fc01e6dab1dc42e794f973b4e0f80afd0bf7
Melakeselam Yitbarek <111082914+melegithubyit@users.noreply.github.com>
2023-10-09 10:26:18 +0300

    Add files via upload

---
+++ pancake_sorting.py
@@ -0,0 +1,21 @@
+class Solution:
+    def pancakeSort(self, arr):
+        def flip(end):
+            start = 0
+            while start < end:
+                arr[start], arr[end] = arr[end], arr[start]
+                start += 1
+                end -= 1
+        N = len(arr)
+        result = []
+        for i in range(N-1, -1, -1):
+            max_val = i
+            for j in range(i, -1, -1):
+                if arr[j] > arr[max_val]:
+                    max_val = j
+            if max_val != i:
+                flip(max_val)
+                flip(i)
+                result.append(max_val + 1)
+                result.append(i + 1)
+        return result
