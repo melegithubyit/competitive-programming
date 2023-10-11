3d91283b4d879031f59d53672dc3ae2f21ee12aa
Melakeselam Yitbarek <111082914+melegithubyit@users.noreply.github.com>
2023-10-11 22:23:46 +0300

    Add files via upload

---
+++ sumof_subArray_minimum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sumSubarrayMins(self, arr):
+        stk = []
+        total = 0
+        n = len(arr)
+        for i in range(n + 1):
+            while stk and (i == n or stk[-1][0] > arr[i]):
+                prev_num, prev_index = stk.pop()
+                if stk:
+                    left = stk[-1][1]
+                else:
+                    left = -1
+                total += (prev_index - left) * (i - prev_index) * prev_num
+            if i < n:
+                stk.append([arr[i], i])
+
+        return total % (10 ** 9 + 7)
