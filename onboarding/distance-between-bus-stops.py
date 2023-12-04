0f2da1074bafdd0a0968a3172b67009460c5e9fb
Melakeselam Yitbarek <111082914+melegithubyit@users.noreply.github.com>
2023-12-04 17:55:22 +0300

    Added solution for Distance Between Bus Stops

--- onboarding/distance-between-bus-stops.py
+++ onboarding/distance-between-bus-stops.py
@@ -2,20 +2,13 @@ class Solution:
     def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
         front_sum = 0
         back_sum = 0
-        
-        if start <= destination:
-            for i in range(start, destination):
-                front_sum += distance[i]
-        
-            back_sum = sum(distance)- front_sum
-            return min(front_sum, back_sum)
+        a,b = min(start,destination), max(start, destination)
 
-        else:
-            for i in range(destination, start):
-                front_sum += distance[i]
-
-            back_sum = sum(distance) - front_sum
-            return min(front_sum, back_sum)
+        for i in range(a, b):
+            front_sum += distance[i]
+    
+        back_sum = sum(distance)- front_sum
+        return min(front_sum, back_sum)
 
 
         
