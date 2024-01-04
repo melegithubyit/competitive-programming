8d6707c7fc41883ae5ea83b4d8dabc6f82893bd5
Melakeselam Yitbarek <111082914+melegithubyit@users.noreply.github.com>
2024-01-04 23:33:37 +0300

    Added solution for Max Number of K-Sum Pairs

---
+++ onboarding/max-number-of-k-sum-pairs.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        counting = dict()
+        operations = 0
+        for i in nums:
+            counting[i] = counting.get(i, 0) + 1
+
+        for num in nums:
+            complement = k - num
+            counting[num] = counting.get(num, 0)
+            counting[complement] = counting.get(complement, 0)
+            if counting[num] > 0 and counting[complement] > 0:
+                if num == complement and counting[num] < 2:
+                    continue
+        
+                counting[num] -= 1
+                counting[complement] -= 1
+                operations += 1
+    
+    
+        return operations
+
+
