c98da59e71df84da41e6940f52b3d6c9e1cc2524
Melakeselam Yitbarek <111082914+melegithubyit@users.noreply.github.com>
2024-02-19 16:48:37 +0300

    Add solution for Score of Parentheses

---
+++ solutions2/leetcode/score-of-parentheses.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def scoreOfParentheses(self, s):
+
+        ans = 0
+        depth = 0
+        
+        for idx, cr in enumerate(s):
+            
+            if cr == '(':
+                depth += 1
+            else:
+                depth -= 1
+                if s[idx-1] == '(': 
+                    ans += 2**depth
+                
+        return ans
+        
