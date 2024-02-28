521fb48b25f94d316f0a6ead7af8f58e70f6dbc5
Melakeselam Yitbarek <111082914+melegithubyit@users.noreply.github.com>
2024-02-28 16:09:10 +0300

    Add solution for Subsets II

---
+++ solutions2/leetcode/subsets-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()  
+        subsets = []
+        def generateSubsets(nums, subset, startIndex):
+            subsets.append(subset[:])
+
+            for i in range(startIndex, len(nums)):
+                if i > startIndex and nums[i] == nums[i - 1]:
+                    continue
+
+                subset.append(nums[i])
+                generateSubsets(nums, subset, i + 1)
+                subset.pop()
+
+        generateSubsets(nums, [], 0)
+        return subsets
