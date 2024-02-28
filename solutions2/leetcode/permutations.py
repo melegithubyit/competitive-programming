d3d7ac553fd6de231538d68bf3b561900ca79b97
Melakeselam Yitbarek <111082914+melegithubyit@users.noreply.github.com>
2024-02-28 11:37:27 +0300

    Add solution for Permutations

---
+++ solutions2/leetcode/permutations.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def permute(self, nums):
+        
+        def backtrack(start):
+            if start == len(nums):
+                result.append(nums[:])
+            else:
+                for i in range(start, len(nums)):
+                    nums[start], nums[i] = nums[i], nums[start]
+                    backtrack(start + 1)
+                    nums[start], nums[i] = nums[i], nums[start]
+        
+        result = []
+        backtrack(0)
+        return result
