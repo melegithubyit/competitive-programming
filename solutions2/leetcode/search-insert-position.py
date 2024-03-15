e670caa867e9a32a90c151d83d7bf702f82ed3a5
Melakeselam Yitbarek <111082914+melegithubyit@users.noreply.github.com>
2024-03-15 18:55:34 +0300

    Add solution for Search Insert Position

---
+++ solutions2/leetcode/search-insert-position.py
@@ -0,0 +1,7 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        nums.insert(len(nums),target)
+        nums.sort()
+        return nums.index(target)
