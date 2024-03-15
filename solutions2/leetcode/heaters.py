a8e220042356f51a81607b60951dda90d0b176c5
Melakeselam Yitbarek <111082914+melegithubyit@users.noreply.github.com>
2024-03-15 19:50:10 +0300

    Add solution for Heaters

---
+++ solutions2/leetcode/heaters.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        houses.sort()
+        heaters.sort()
+        radius = 0
+
+        for house in houses:
+            index = bisect.bisect_left(heaters, house)
+            if index == 0:
+                radius = max(radius, heaters[0] - house)
+            elif index == len(heaters):
+                radius = max(radius, house - heaters[-1])
+            else:
+                radius = max(radius, min(heaters[index] - house, house - heaters[index - 1]))
+
+        return radius
